9051978d9c74cec29dd282fcc97f517752b1cd78
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2020-10-22 22:13:12 +0200

    initilaizing with header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="Ahmed Ali">
+        <title>Shezlong Home Page</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Bootstrap css -->
+        <link rel="stylesheet" href={{ asset('bootstrap/css/bootstrap.css') }}>
+    </head>
+    <body>
+
+        <header id="header">
+            <!-- navbar -->
+            
+            <!-- end of navbar -->
+        </header>
+
+        <script src={{asset('bootstrap/js/bootstrap.js')}}></script>
+        <script src={{asset('bootstrap/js/jquery.js')}}></script>
+    </body>
+</html>
