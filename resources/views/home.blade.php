1579a8dc2583758524ba96f55e1753063c069f4e
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2020-10-23 13:33:01 +0200

    chat added correctly

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,10 @@
             @include('navbar')
         </header>
 
+        <section class="banner">
+            
+        </section>
+
         <div class="message">
             <img class="img_msg" src="/img/message.svg" alt="message" style="width:50px;height:50px;">
         </div>
@@ -12,8 +16,14 @@
             <a href="#">
                 <img src="/img/logo-en.png" alt="logo" style="width:80px;height:30px;">
             </a>
-            <h4 class="chat-heading mx-1 my-3">Hi there !  <span><img src="/img/hello.svg" alt="hello" style="width: 30px;height:30px;"></span></h4>
-            <p class="chat-subtitle mx-1">This chat belongs to support team , please contact us for help.</p>
+            <h5 class="chat-heading ml-2 mb-2">Hi there !  <span><img src="/img/hello.svg" alt="hello" style="width: 30px;height:30px;"></span></h5>
+            <p class="chat-subtitle ml-2">This chat belongs to support team , please contact us for help.</p>
+            <div class="chat-body">
+                <p style="color: black;font-size:14px;font-weight:bold;">Start new conversation</p>
+                <img class="mr-1" src="/img/me.png" alt="customer" style="width: 35px;height:35px;float:left;"> 
+                <p style="font-size:12px;color:gray">Our usual reply time<br><i class="fa fa-clock-o mr-1" style="color: #3DA8C0"></i><strong>A few minutes</strong></p>
+                <a href="mailTo:ahmedalimohamed1994@gmail.com" style="text-decoration:none;"><button type="button" class="btn"><i class="fa fa-send" style="font-size: 14px"></i> send us message</button></a>
+            </div>
         </div>
 
         <footer class="footer py-5" id="footer">
