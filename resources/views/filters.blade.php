9997821cd07e596a1bcd924d86cad8ca5ec9eaf1
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2020-10-23 20:52:47 +0200

    filters done

--- resources/views/filters.blade.php
+++ resources/views/filters.blade.php
@@ -1,5 +1,34 @@
 <div class="container">
-    <div class="row col-3 flex-row-reverse mx-auto">
-        
+    <div class="row flex-row-reverse mx-auto">
+        <div class="input-group col-3">
+           <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fa fa-filter"></i></span>
+           </div>
+           <select class="custom-select">
+              <option selected value="lth">{{__('fees_LH')}}</option>
+              <option value="htl">{{__('fees_HL')}}</option>
+              <option value="rate">{{__('top_rated')}}</option>
+           </select>
+        </div>
+        <div class="input-group col-3">
+           <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+              <option selected value="all">{{__('all_spec')}}</option>
+              <option value="Child disorders">{{__('child_disorder')}}</option>
+              <option value="Adolescence disorders">{{__('adolescence')}}</option>
+              <option value="Depression">{{__('depression')}}</option>
+              <option value="Anxiety disorders and obsession">{{__('anxiety')}}</option>
+              <option value="Marriage and relationships">{{__('marriage')}}</option>
+              <option value="Psychotic disorders">{{__('psychotic')}}</option>
+              <option value="Addiction">{{__('addiction')}}</option>
+              <option value="Sexual disorders">{{__('sexual')}}</option>
+              <option value="Old age disorders">{{__('old_age')}}</option>
+           </select>
+        </div>
+        <div class="input-group col-3">
+           <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fa fa-search"></i></span>
+           </div>
+           <input type="text" class="form-control" placeholder={{__('therapist_name')}} aria-label="Username" aria-describedby="basic-addon1">
+        </div>
     </div>
 </div>
