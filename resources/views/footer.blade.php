f369b218fc0efc809b92324b2707f46e63520896
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2020-10-23 04:25:40 +0200

    finishing footer section

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,55 @@
+<!-- footer section -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3 col-md-4 px-5 my-5">
+            <p class="footer-section-heading mb-4">
+                Shezlong Blog
+            </p>
+            <a class="footer-section-item" href="#">
+                Terms and Conditions 
+            </a>
+            <a class="footer-section-item" href="#">
+                Privacy and Policy
+            </a>
+            <a class="footer-section-item" href="#">
+                Careers
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-4 px-5 my-5">
+            <p class="footer-section-heading mb-4">
+                Learn More
+            </p>
+            <a class="footer-section-item" href="#">
+                Blog 
+            </a>
+            <a class="footer-section-item" href="#">
+                Emergenecy Numbers
+            </a>
+        </div>
+        <div class="col-lg-2 col-md-4 px-5 my-5">
+            <p class="footer-section-heading mb-4">
+                Therapists
+            </p>
+            <a class="footer-section-item" href="#">
+                Join Us
+            </a>
+        </div>
+        <div class="col-lg-4 col-md-4 px-5 my-5">
+            <div class="mb-4" style="text-align: center;">
+               <a class="mx-2" href="#"><img src="/img/facebook.svg" alt="facebook" style="width:30px;height:30px;"></a>
+               <a class="mx-2" href="#"><img src="/img/instagram.svg" alt="instagram" style="width:30px;height:30px;"></a>
+            </div>
+            <p class="footer-item">
+                All Rights Reserved to Shezlong 2020
+            </p>
+            <div class="mb-2" style="text-align: center;">
+               <a href="#"><img src="/img/logo-en.png" alt="logo" style="width:150px;height:57px;"></a>
+            </div>
+            <div style="text-align: center;">
+               <a class="mx-2" href="#"><img src="/img/ismho.png" alt="ISMHO" style="width:55px;height:40px;"></a>
+               <a class="mx-2" href="#"><img src="/img/ssl.png" alt="ISMHO" style="width:100px;height:35px;"></a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end of footer -->
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,17 @@
     <body>
 
         <header id="header">
-            @extends('navbar')
+            @include('navbar')
         </header>
 
-        <footer id="footer">
-            @extends('footer')
+        
+
+        <footer class="footer py-5" id="footer">
+            @include('footer')
         </footer>
 
         <script src={{asset('bootstrap/jquery/jquery.js')}}></script>
         <script src={{asset('bootstrap/js/bootstrap.js')}}></script>
+        <script src={{asset('js/behaviors.js')}}></script>
     </body>
 </html>
