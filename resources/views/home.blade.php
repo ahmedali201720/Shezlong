9eac9b6b6b4708502be711f277b229477e69ea9c
Ahmed Ali <ahmedalimohamed1994@gmail.com>
2020-10-23 02:01:28 +0200

    adding body section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,10 @@
             @extends('navbar')
         </header>
 
+        <footer id="footer">
+            @extends('footer')
+        </footer>
+
         <script src={{asset('bootstrap/jquery/jquery.js')}}></script>
         <script src={{asset('bootstrap/js/bootstrap.js')}}></script>
     </body>
